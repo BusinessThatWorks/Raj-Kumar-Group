0cb74bfb8509917dfdf62193078fa19f568b8b86
clapgrowdev2-sudo <clapgrowdev2@clapgrow.com>
2025-12-03 17:02:00 +0530

    Completed the Damage Vehicle assess

--- rkg/rkg/doctype/damage_assessment/damage_assessment.js
+++ rkg/rkg/doctype/damage_assessment/damage_assessment.js
@@ -20,8 +20,39 @@ frappe.ui.form.on("Damage Assessment", {
 
 frappe.ui.form.on("Damage Assessment Item", {
 	item_code(frm, cdt, cdn) {
-		// Clear serial_no when item_code changes to avoid mismatch
 		let row = locals[cdt][cdn];
+		
+		// Clear serial_no when item_code changes to avoid mismatch
 		frappe.model.set_value(cdt, cdn, "serial_no", "");
+		frappe.model.set_value(cdt, cdn, "damaged_qty", 0);
+		
+		if (row.item_code) {
+			// Fetch total_qty_received from Serial No doctype
+			frappe.call({
+				method: "rkg.rkg.doctype.damage_assessment.damage_assessment.get_serial_no_count",
+				args: {
+					item_code: row.item_code
+				},
+				callback: function(r) {
+					if (r.message !== undefined) {
+						frappe.model.set_value(cdt, cdn, "total_qty_received", r.message);
+					}
+				}
+			});
+		} else {
+			frappe.model.set_value(cdt, cdn, "total_qty_received", 0);
+		}
+	},
+	
+	serial_no(frm, cdt, cdn) {
+		let row = locals[cdt][cdn];
+		
+		if (row.serial_no) {
+			// When serial_no is selected, set damaged_qty = 1
+			// (each serial no represents 1 unit being marked as damaged)
+			frappe.model.set_value(cdt, cdn, "damaged_qty", 1);
+		} else {
+			frappe.model.set_value(cdt, cdn, "damaged_qty", 0);
+		}
 	}
 });
--- rkg/rkg/doctype/damage_assessment/damage_assessment.py
+++ rkg/rkg/doctype/damage_assessment/damage_assessment.py
@@ -1,6 +1,16 @@
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class DamageAssessment(Document):
 	pass
+
+
+@frappe.whitelist()
+def get_serial_no_count(item_code):
+	"""Get total count of Serial Nos for a given item_code"""
+	if not item_code:
+		return 0
+	
+	count = frappe.db.count("Serial No", filters={"item_code": item_code})
+	return count or 0
