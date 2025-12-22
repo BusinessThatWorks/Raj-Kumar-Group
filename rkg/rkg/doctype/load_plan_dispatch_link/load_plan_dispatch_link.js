1258a92d52638be5e5e87e1cc0cb16cffe6cb655
clapgrowdev2-sudo <clapgrowdev2@clapgrow.com>
2025-12-22 17:48:32 +0530

    Updated the Load Plan DOctype to SHow the Related Dispatches

---
+++ rkg/rkg/doctype/load_plan_dispatch_link/load_plan_dispatch_link.js
@@ -0,0 +1,11 @@
+frappe.ui.form.on("Load Plan Dispatch Link", {
+	load_dispatch(frm, cdt, cdn) {
+		// Make the load_dispatch field clickable to open the dispatch document
+		const row = locals[cdt][cdn];
+		if (row.load_dispatch) {
+			// This will be handled by Frappe's default link behavior
+			// But we can add custom behavior if needed
+		}
+	}
+});
+
