58796c82210b5062cab07e9a3a806c71144ff26b
sumandas-clapgrow <suman.das@clapgrow.com>
2026-01-19 15:43:09 +0530

    year and month data import

---
+++ rkg/rkg/doctype/month/month.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, beetashoke.chakraborty@clapgrow.com and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class month(Document):
+	pass
