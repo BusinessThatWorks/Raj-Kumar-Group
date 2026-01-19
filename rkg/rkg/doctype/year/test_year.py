a2e2e2a36346b398d36cdf5991bdf103555aa9c1
sumandas-clapgrow <suman.das@clapgrow.com>
2026-01-19 15:07:48 +0530

    change year

---
+++ rkg/rkg/doctype/year/test_year.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, beetashoke.chakraborty@clapgrow.com and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestYear(FrappeTestCase):
+	pass
